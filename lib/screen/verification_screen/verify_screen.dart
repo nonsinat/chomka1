6e5ceabf016247c7125e30d72d3bdacfaad3f75f
NonSinat <sinat.non@student.niptict.edu.kh>
2021-06-01 21:32:58 +0700

    add file to each folder

---
+++ lib/screen/verification_screen/verify_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerificationScreen extends StatefulWidget {
+  @override
+  _VerificationScreenState createState() => _VerificationScreenState();
+}
+
+class _VerificationScreenState extends State<VerificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
